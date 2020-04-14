a10cd5870b477aa7bf0cc939e325bd1abcd3900b
imxande <alerodriguezlarosa@gmail.com>
2020-04-14 10:36:26 -0400

    test_iterative pass and clean ups

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -20,16 +20,20 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    # loop through n-1 elements
     for i in range(0, len(arr) - 1):
+        # need a temporary variable to swap
         temp = arr[i]
+        # in order to compare I need to loop again
         for j in range(0, len(arr) - 1):
+            # checking if right hand side is less the left hand side
             if arr[j] > arr[j + 1]:
+                # stored the right hand side value into variable so I can use that spot
                 temp = arr[j + 1]
+                # copy the left hand side value into the right hand side one
                 arr[j + 1] = arr[j]
+                # the left hand side value now gets the value stored in the temporary variable to complete the swap
                 arr[j] = temp
-        
-        
-
 
     return arr
 
